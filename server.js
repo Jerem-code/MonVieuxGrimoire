e336a9b4b952a7a64d0bf0456814bd4c62797fb4
Jerem-code <jeremy.pitton@wanadoo.fr>
2025-01-26 17:04:24 +0100

    feat: setup basic Express server

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
-const express = require("express");
+const http = require("http");
+const app = require("./app");
 
-const app = express();
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-app.use(express.json());
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || 4000);
+app.set("port", port);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
-app.get("/api/test", (req, res) => {
-  res.json({ message: "Votre serveur fonctionne correctement!" });
-});
-
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Serveur démarré sur le port ${port}`);
-});
-
-module.exports = app;
+server.listen(port);
