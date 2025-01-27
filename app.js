4dd06ca6c9c0c8169ce050e940f59c6d7d8a8d5f
Jerem-code <jeremy.pitton@wanadoo.fr>
2025-01-27 16:05:42 +0100

    feat: add MongoDB database connection

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// Importation du modèle Book
+const Book = require("./models/Book");
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(express.json());
 
@@ -17,8 +26,34 @@ app.use((req, res, next) => {
   next();
 });
 
+// Route de test
 app.get("/api/test", (req, res) => {
   res.json({ message: "Votre serveur fonctionne correctement!" });
+});
+
+// Routes pour tests CRUD sur POSTMAN
+app.post("/api/books", (req, res) => {
+  const book = new Book({
+    ...req.body,
+  });
+  book
+    .save()
+    .then(() => res.status(201).json({ message: "Livre enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Récupérer tous les livres
+app.get("/api/books", (req, res) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Supprimer un livre
+app.delete("/api/books/:id", (req, res) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Livre supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
