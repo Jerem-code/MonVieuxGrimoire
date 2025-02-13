cc5e7afdd45258d260bf778f1e16be3315581986
Jerem-code <jeremy.pitton@wanadoo.fr>
2025-02-13 23:49:57 +0100

    feat : added existing email check on signup

--- controllers/user.js
+++ controllers/user.js
@@ -2,22 +2,26 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() =>
-          res.status(201).json({ message: "Utilisateur créé avec succès" })
-        )
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+exports.signup = async (req, res, next) => {
+  try {
+    // Vérifier si l'email existe déjà
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).json({ error: "Cet email est déjà utilisé" });
+    }
+
+    // Si l'email n'existe pas, créer l'utilisateur
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+
+    await user.save();
+    res.status(201).json({ message: "Utilisateur créé avec succès" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 exports.login = (req, res, next) => {
